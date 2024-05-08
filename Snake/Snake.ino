1fa57981d53bf1db505908cb5ebd32355e8e1b62
Adrileg <160527060+Adrileg@users.noreply.github.com>
2024-05-08 20:00:20 +0200

    Update Snake.ino
    Hello

--- Snake/Snake.ino
+++ Snake/Snake.ino
@@ -1,6 +1,7 @@
 #include <MD_MAX72xx.h>
-
-#define HARDWARE_TYPE MD_MAX72XX::DR0CR0RR1_HW
+#define MAX_DEVICES 4
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#include <LinkedList.h>
 
 
 MD_MAX72XX cartel = MD_MAX72XX(HARDWARE_TYPE, 11, 13, 10, 4);
@@ -13,13 +14,32 @@ int comida_Y;
 int Fila;
 int Columna;
 
+LinkedList<int> cola_X;  // Lista enlazada para la cola X de la serpiente
+LinkedList<int> cola_Y;  // Lista enlazada para la cola Y de la serpiente
+
 bool Ha_muerto;
 bool Ha_comido;
 
 int snake_head[2];
 
 bool pantalla[8][32];
+ 
+void actualizar_snake() {
+  // Agrega la posición actual de la cabeza a la cola
+  cola_X.add(snake_head[0]);
+  cola_Y.add(snake_head[1]);
 
+  // Mueve la serpiente
+  if (direccion == 0) snake_head[0]--;       // Arriba
+  else if (direccion == 1) snake_head[0]++;  // Abajo
+  else if (direccion == 2) snake_head[1]--;  // Izquierda
+  else if (direccion == 3) snake_head[1]++;  // Derecha
+  // Si no ha comido, elimina el último segmento de la cola
+  int tail_X = cola_X.remove(0);
+  int tail_Y = cola_Y.remove(0);
+  pantalla[tail_Y][tail_X] = false;
+  pantalla[snake_head[1]][snake_head[0]] = true;
+}
 
 void Crecer_serpiente() {
 }
@@ -72,8 +92,7 @@ void updatePantalla() {
   }
 }
 
-void actualizar_snake() {
-}
+
 
 void Read_joystick() {
   // 0 = izquierda, 1 = decbar, 2 = arriba, 3= abajo
